0bb831e78645a9b7393fa8e102b48f99507b7823
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-12 17:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	let ropes = document.querySelector("#rope-lengths").value;
+	let result = document.querySelector("#result");
+	let ropeLen = ropes.split(',');
+	ropeLen.sort((a,b)=>{return a-b});
+	// console.log(ropes, ropeLen);
+	let all = [];
+	ropeLen.forEach((e)=>{
+		let i = parseInt(e);
+		all.push(i);
+	});
+	let res = 0;
+	while(all.length>1){
+		let a = all.shift();
+		let b = all.shift();
+		res += a+b;
+		all.push(a+b);
+		all.sort((a,b)=>{return a-b});
+	}
+	// console.log(res);
+	result.innerText = res;
+	
 }  
